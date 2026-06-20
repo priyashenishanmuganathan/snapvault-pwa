1319801a6185a3610080417fbcafd3a3c77a6f3e
priyashenishanmuganathan <priyashenishanmuganathan@gmail.com>
2026-06-20 21:14:23 +0800

    Final Product

---
+++ src/data/rewards.js
@@ -0,0 +1,51 @@
+export const rewards = [
+
+  {
+    id: 1,
+    name:
+      "SnapVault Elite T-Shirt",
+    points: 1000,
+    type: "physical",
+  },
+
+  {
+    id: 2,
+    name:
+      "SnapVault Premium Hoodie",
+    points: 2500,
+    type: "physical",
+  },
+
+  {
+    id: 3,
+    name:
+      "SnapVault Laptop Bag",
+    points: 3500,
+    type: "physical",
+  },
+
+  {
+    id: 4,
+    name:
+      "SnapVault Power Bank",
+    points: 5000,
+    type: "physical",
+  },
+
+  {
+    id: 5,
+    name:
+      "Starbucks Voucher",
+    points: 300,
+    type: "voucher",
+  },
+
+  {
+    id: 6,
+    name:
+      "ZUS Voucher",
+    points: 200,
+    type: "voucher",
+  },
+
+];
